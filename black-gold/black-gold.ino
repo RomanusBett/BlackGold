c9fbab854ede1eafc209d9fe9d0b2511162e4348
romanusbett <bettromanus@gmail.com>
2025-04-10 18:53:16 +0300

    add backend setup

--- black-gold/black-gold.ino
+++ black-gold/black-gold.ino
@@ -1,21 +1,23 @@
 const int ledPin = 2;        
 const int photodiodePin = 25; 
+const int feedBackPin = 27;
 float baseline = 0;        
 bool oilDetected = false;   
 
 void setup() {
     pinMode(ledPin, OUTPUT);
+    pinMode(feedBackPin, OUTPUT);
     Serial.begin(9600);
 }
 
 void loop() {
     int ambientLight = analogRead(photodiodePin);
-
     digitalWrite(ledPin, HIGH);  
     delay(10);
     int sensorReading = analogRead(photodiodePin);
     digitalWrite(ledPin, LOW);   
     delay(500);  
+
 
     if (ambientLight >= 700 || ambientLight <= 50) return;
     if (sensorReading >= 700 || sensorReading <= 50) return;
@@ -26,8 +28,12 @@ void loop() {
     int threshold = baseline - 50; 
 
     if (!oilDetected && finalReading < threshold) {
+        digitalWrite(feedBackPin, HIGH);
+        delay(3000);
         oilDetected = true;
         Serial.println("🛢️ Oil Detected!");
+        digitalWrite(feedBackPin, LOW);
+        delay(2000);
     } else {
         Serial.println("✅ No Oil Detected.");
     }
